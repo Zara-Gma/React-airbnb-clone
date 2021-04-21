5b1f79b1638aef10639d6935cdaec1c47cd547bf
Zara-Gma <43872052+Zara-Gma@users.noreply.github.com>
2021-04-21 00:21:46 -0700

    updates

--- src/Header.js
+++ src/Header.js
@@ -4,15 +4,18 @@ import SearchIcon from "@material-ui/icons/Search";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import LanguageIcon from "@material-ui/icons/Language";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header_icon"
-        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.neTNJfzreMdI2zhzWpugogHaCr%26pid%3DApi&f=1"
-        alt="logo"
-      />
+      <Link to="/">
+        <img
+          className="header__icon"
+          src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+          alt=""
+        />
+      </Link>
       <div className="header_center">
         <input type="text" />
         <SearchIcon />
